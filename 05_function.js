def1a7129595b0bb61d5e7feda5d94167ccaa60c
anuz <95576641+anuz-net@users.noreply.github.com>
2025-03-30 21:44:55 +0545

    Function learned

---
+++ 05_function.js
@@ -0,0 +1,22 @@
+function add ( x, y ){
+    console.log("Done");
+    return x + y ;
+}
+
+a=2;
+b=10;
+c=94;
+
+console.log(add(a,b));
+console.log(add(b,c)/2);
+
+function hero(x, y,z){
+    return x+y+(z/2);
+}
+
+i=0;
+j=1;
+k=2;
+
+console.log(hero(i,j,k));
+
