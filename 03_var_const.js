892c55274cbdefc444f1c03761bca65a70d62bd0
anuz <95576641+anuz-net@users.noreply.github.com>
2025-03-30 21:27:33 +0545

    datatype learned

---
+++ 03_var_const.js
@@ -0,0 +1,12 @@
+console.log(" Java Script : Let, Const and Var ");
+
+var a = "Anuz"
+let b = 2
+const c = 3
+
+console.log(a,b,c)
+
+{ // Block Scope..
+    let a=0;
+    console.log(a);
+}
---
+++ 04_datatypes.js
@@ -0,0 +1,26 @@
+
+// Data Types in JS
+// nn bb ss uu 
+
+let a = null ;
+let b = undefined;
+let c = "Anuz";
+let d = 2;
+let e  = true;
+let f = BigInt("512");
+let g = Symbol("abc");
+console.log(a,b,c,d,e,f,g);
+/**
+ * Demonstrates various data types in JavaScript.
+ * 
+ * Variables:
+ * - `a` (null): Represents the intentional absence of any object value.
+ * - `b` (undefined): Represents a variable that has been declared but not assigned a value.
+ * - `c` (string): A sequence of characters, here assigned the value "Anuz".
+ * - `d` (number): A numeric data type, here assigned the value 2.
+ * - `e` (boolean): A logical data type that can be either true or false, here assigned true.
+ * - `f` (BigInt): A numeric data type for arbitrarily large integers, here assigned the value 512.
+ * - `g` (Symbol): A unique and immutable primitive value, here created with the description "abc".
+ * 
+ * The `console.log` statement outputs all these variables to the console.
+ */
