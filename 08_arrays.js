3e867d15b2f1671042f7bd6380f252787f0c0fb3
anuz <95576641+anuz-net@users.noreply.github.com>
2025-03-31 16:57:13 +0545

    Array Learned

---
+++ 08_arrays.js
@@ -0,0 +1,8 @@
+let sea = ["Water", "Sea", "Ocean"];
+console.log(sea[0]);
+
+let Marks = ["4", "9", "8"];   //Indexing Statrt from 0
+console.log(Marks[2]);
+
+
+console.log(sea.length); //Finding the length of an arrayyy !! 
